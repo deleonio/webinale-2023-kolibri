6e8996b0aed189726e71fafe297108dd82c922d3
Martin Oppitz <github@martinoppitz.com>
2023-05-25 02:24:26 +0200

    initial commit

---
+++ styles/index.ts
@@ -0,0 +1,20 @@
+import { KoliBriDevHelper, register } from '@public-ui/components';
+import { BPA, BMF, DESYv2, ECL_EC, ECL_EU, ITZBund, MAPZ, ZOLLv2 } from '@public-ui/themes';
+import { TH } from '@public-oss/kolibri-themes';
+import './base.css';
+
+register(
+	[BPA, BMF, DESYv2, ECL_EC, ECL_EU, ITZBund, MAPZ, ZOLLv2, TH],
+	() => {
+		return new Promise((resolve) => resolve());
+	},
+	{
+		theme: {
+			detect: 'auto',
+		},
+	}
+)
+	.then(() => {
+		document.body.dataset.theme = 'itzbund';
+	})
+	.catch(() => console.warn);
